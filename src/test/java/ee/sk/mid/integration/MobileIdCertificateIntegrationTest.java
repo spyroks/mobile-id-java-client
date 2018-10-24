ae2be0420a1be4fa355f3cc30ebf28d68b500fc7
Denis Rästas <denis.rastas@nortal.com>
2018-10-24 15:58:09 +0300

    DDS-2507 added certificate integration tests

---
+++ src/test/java/ee/sk/mid/integration/MobileIdCertificateIntegrationTest.java
@@ -0,0 +1,78 @@
+package ee.sk.mid.integration;
+
+import ee.sk.mid.MobileIdClient;
+import ee.sk.mid.exception.CertificateNotPresentException;
+import ee.sk.mid.exception.ExpiredException;
+import ee.sk.mid.exception.ParameterMissingException;
+import ee.sk.mid.exception.UnauthorizedException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.cert.X509Certificate;
+
+import static ee.sk.mid.mock.MobileIdRestServiceRequestDummy.*;
+import static ee.sk.mid.mock.TestData.*;
+
+public class MobileIdCertificateIntegrationTest {
+
+    private MobileIdClient client;
+
+    @Before
+    public void setUp() {
+        client = new MobileIdClient();
+        client.setRelyingPartyUUID(VALID_RELYING_PARTY_UUID);
+        client.setRelyingPartyName(VALID_RELYING_PARTY_NAME);
+        client.setHostUrl(HOST_URL);
+    }
+
+    @Test
+    public void getCertificate() {
+        X509Certificate certificate = createCertificate(client);
+
+        assertCertificateCreated(certificate);
+    }
+
+    @Test(expected = CertificateNotPresentException.class)
+    public void getCertificate_whenCertificateNotPresent_shouldThrowException() {
+        makeCertificateRequest(client, VALID_PHONE_NOT_MID_CLIENT, VALID_NAT_IDENTITY_NOT_MID_CLIENT);
+    }
+
+    @Test(expected = ExpiredException.class)
+    public void getCertificate_whenInactiveCertificateFound_shouldThrowException() {
+        makeCertificateRequest(client, VALID_PHONE_NOT_ACTIVE_CERTIFICATE, VALID_NAT_IDENTITY_NOT_ACTIVE_CERTIFICATE);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongPhoneNumber_shouldThrowException() {
+        makeCertificateRequest(client, WRONG_PHONE, VALID_NAT_IDENTITY);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongNationalIdentityNumber_shouldThrowException() {
+        makeCertificateRequest(client, VALID_PHONE, WRONG_NAT_IDENTITY);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongRelyingPartyUUID_shouldThrowException() {
+        client.setRelyingPartyUUID(WRONG_RELYING_PARTY_UUID);
+        makeCertificateRequest(client, VALID_PHONE, VALID_NAT_IDENTITY);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongRelyingPartyName_shouldThrowException() {
+        client.setRelyingPartyName(WRONG_RELYING_PARTY_NAME);
+        makeCertificateRequest(client, VALID_PHONE, VALID_NAT_IDENTITY);
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void getCertificate_withUnknownRelyingPartyUUID_shouldThrowException() {
+        client.setRelyingPartyUUID(UNKNOWN_RELYING_PARTY_UUID);
+        makeCertificateRequest(client, VALID_PHONE, VALID_NAT_IDENTITY);
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void getCertificate_withUnknownRelyingPartyName_shouldThrowException() {
+        client.setRelyingPartyName(UNKNOWN_RELYING_PARTY_NAME);
+        makeCertificateRequest(client, VALID_PHONE, VALID_NAT_IDENTITY);
+    }
+}
---
+++ src/test/java/ee/sk/mid/rest/integration/MobileIdRestConnectorCertificateIntegrationTest.java
@@ -0,0 +1,69 @@
+package ee.sk.mid.rest.integration;
+
+import ee.sk.mid.exception.ParameterMissingException;
+import ee.sk.mid.exception.UnauthorizedException;
+import ee.sk.mid.rest.MobileIdConnector;
+import ee.sk.mid.rest.MobileIdRestConnector;
+import ee.sk.mid.rest.dao.request.CertificateRequest;
+import ee.sk.mid.rest.dao.response.CertificateChoiceResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static ee.sk.mid.mock.MobileIdRestServiceRequestDummy.createCertificateRequest;
+import static ee.sk.mid.mock.MobileIdRestServiceRequestDummy.createValidCertificateRequest;
+import static ee.sk.mid.mock.MobileIdRestServiceResponseDummy.assertCertificateChosen;
+import static ee.sk.mid.mock.TestData.*;
+
+public class MobileIdRestConnectorCertificateIntegrationTest {
+
+    private MobileIdConnector connector;
+
+    @Before
+    public void setUp() {
+        connector = new MobileIdRestConnector(HOST_URL);
+    }
+
+    @Test
+    public void getCertificate() {
+        CertificateRequest request = createValidCertificateRequest();
+        CertificateChoiceResponse response = connector.getCertificate(request);
+
+        assertCertificateChosen(response);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongPhoneNumber_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(VALID_RELYING_PARTY_UUID, VALID_RELYING_PARTY_NAME, WRONG_PHONE, VALID_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongNationalIdentityNumber_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(VALID_RELYING_PARTY_UUID, VALID_RELYING_PARTY_NAME, VALID_PHONE, WRONG_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongRelyingPartyUUID_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(WRONG_RELYING_PARTY_UUID, VALID_RELYING_PARTY_NAME, VALID_PHONE, VALID_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+
+    @Test(expected = ParameterMissingException.class)
+    public void getCertificate_withWrongRelyingPartyName_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(VALID_RELYING_PARTY_UUID, WRONG_RELYING_PARTY_NAME, VALID_PHONE, VALID_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void getCertificate_withUnknownRelyingPartyUUID_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(VALID_RELYING_PARTY_UUID, UNKNOWN_RELYING_PARTY_NAME, VALID_PHONE, VALID_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void getCertificate_withUnknownRelyingPartyName_shouldThrowException() {
+        CertificateRequest request = createCertificateRequest(UNKNOWN_RELYING_PARTY_UUID, VALID_RELYING_PARTY_NAME, VALID_PHONE, VALID_NAT_IDENTITY);
+        connector.getCertificate(request);
+    }
+}
